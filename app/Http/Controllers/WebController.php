12d394ab4ed609a634d205537fa33d85ca413f86
Saepul Ramdan <saepulramdan244@gmail.com>
2025-08-12 16:26:38 +0700

    update

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WebController extends Controller
 {
@@ -11,4 +12,37 @@ class WebController extends Controller
         return view('welcome');
     }
 
+    public function getPeserta(Request $request)
+    {
+        $search = $request->input('search', '');
+
+        $query = DB::table('pesertas')
+            ->join('events', 'pesertas.event_id', '=', 'events.id')
+            ->select(
+                'pesertas.id',
+                'pesertas.callsign',
+                'pesertas.nama_peserta',
+                'pesertas.waktu_checkin',
+                'pesertas.nomor_sertifikat',
+                'pesertas.created_at',
+                'events.nama_event',
+                'events.tanggal_mulai',
+                'events.tanggal_selesai',
+                'events.kode_sertifikat'
+            );
+
+        if ($search) {
+            $query->where(function($q) use ($search) {
+                $q->where('pesertas.callsign', 'like', "%{$search}%")
+                  ->orWhere('pesertas.nama_peserta', 'like', "%{$search}%")
+                  ->orWhere('pesertas.nomor_sertifikat', 'like', "%{$search}%")
+                  ->orWhere('events.nama_event', 'like', "%{$search}%");
+            });
+        }
+
+        $pesertas = $query->orderBy('pesertas.created_at', 'desc')
+                         ->paginate(10);
+
+        return response()->json($pesertas);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 // Default route ke landing page
 Route::get('/', [WebController::class, 'index'])->name('home');
+Route::get('/peserta/data', [WebController::class, 'getPeserta'])->name('peserta.data');
 
 Route::middleware(['auth', 'web'])->group(function () {
     Route::get('/dashboard', fn() => view('dashboard'))->name('dashboard');
